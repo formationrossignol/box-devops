edc42a51b94cb75c25665c6e3659852456dd9167
formationrossignol <92857075+formationrossignol@users.noreply.github.com>
2022-01-27 09:20:45 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,30 +25,9 @@ sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
-## Kubectl
-echo -e "---------------------- Install Kubectl ----------------------\n"
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-sudo yum install -y kubectl
-
-## Helm
-echo -e "---------------------- Install Helm ----------------------\n"
-sudo snap install helm --classic
-
-## Openstack CLI
-echo -e "---------------------- Install Openstack CLI ----------------------\n"
-sudo subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rpms --enable=rhel-8-for-x86_64-supplementary-rpms --enable=codeready-builder-for-rhel-8-x86_64-rpms
-sudo dnf install -y https://www.rdoproject.org/repos/rdo-release.el8.rpm
-sudo yum upgrade -y
-sudo yum install -y python3-openstackclient
-sudo yum install -y openstack-selinux
+## Docker compose
+sudo curl –L "https://github.com/docker/compose/releases/download /1.26.2/docker-compose-$(uname -s)-$(uname -m)" –o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Post Install
 echo -e "############### Post Install ###############\n"
